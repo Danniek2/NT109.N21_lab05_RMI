09c108a216cf138a79df8d633ff85bb91e5a00fc
Danniek2 <dannie022023@gmail.com>
2023-06-03 13:37:42 +0700

    update1

--- RMIClient/src/rmiclient/RMIClient.java
+++ RMIClient/src/rmiclient/RMIClient.java
@@ -6,29 +6,43 @@ package rmiclient;
 
 import java.rmi.Naming;
 import rmiserver.IMath;
-
+import java.util.Scanner;
+import java.io.IOException;
 /**
  *
  * @author Admin
  */
 public class RMIClient {
+    private static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        confixlog n = new confixlog();
+        System.out.print("Nhap so thu nhat, a = ");
+        int a = RMIClient.scanner.nextInt();
+        n.Logs("Nhap vao so thu nhat");
+        System.out.print("Nhap so thu hai, b = ");
+        int b = RMIClient.scanner.nextInt();
+        n.Logs("Nhap vao so thu hai");
         // TODO code application logic here
         try {
             IMath imath = (IMath) Naming.lookup("rmi://localhost:5000/MyServices");
             
-            int tong = imath.add(2,4);
-            int hieu = imath.sub(2, 4);
-            int tich = imath.mul(2, 4);
-            int thuong = imath.div(4, 2);
+            int tong = imath.add(a, b);
+            int hieu = imath.sub(a, b);
+            int tich = imath.mul(a, b);
+            int thuong = imath.div(a, b);
             System.out.println("Tong 2 so: " + tong);
+            n.Logs("ket qua cua phep tinh tong hai so");
             System.out.println("Hieu 2 so: " + hieu);
+            n.Logs("ket qua cua phep tinh hieu hai so");
             System.out.println("Tich 2 so: " + tich);
+            n.Logs("ket qua cua phep tinh tich hai so");
             System.out.println("Thuong 2 so: " + thuong);
+            n.Logs("ket qua cua phep tinh thuong hai so");
         }
         catch (Exception e) {
         System.err.println("Error: " + e.getMessage());
+        n.Logs("nhap vao bi loi");
         }
     }
 }
---
+++ RMIClient/src/rmiclient/confixlog.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rmiclient;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+/**
+ *
+ * @author Admin
+ */
+public class confixlog {
+    public confixlog()
+    {
+    
+    }
+    public void Logs(String s) throws IOException{
+        Date date = new Date();
+        
+        try{
+            //tạo tệp và lưu vào ổ D:\\JavaTest\\test22_5 với tên là Logs
+               File foder = new File("D:\\JavaTest\\RMI\\RMIClient\\Logs");
+                if (!foder.exists()) {
+                if (foder.mkdir()) { }
+                 }
+            //tạo file txt để ghi log và lưu vào tệp vừa tạo
+               File file =new File("D:\\JavaTest\\RMI\\RMIClient\\Logs\\DangTrongDanh_20521160.txt");
+
+               if(!file.exists()){
+                  file.createNewFile();
+               }
+
+               //nối thêm nội dung vào tệp
+               FileWriter fw = new FileWriter(file,true);
+               //ghi bufferedWriter để cho hiệu suất tốt hơn
+               BufferedWriter bw = new BufferedWriter(fw);
+               bw.write(date+": " + s + "\n");
+               //đóng luồng
+               bw.close();
+
+             }catch(IOException ioe){
+                System.out.println(ioe);
+              }
+    }
+    
+}
